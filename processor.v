f5c6a0d33927e9b34b0d918209acdb5277589dc0
elliothha <elliot.ha@duke.edu>
2024-04-14 06:20:33 -0400

    Need bypassing

--- processor.v
+++ processor.v
@@ -76,6 +76,7 @@ module processor(
 
     //  FETCH STAGE output wires
     wire [31:0] PC_insn;    // 32-bit ISA-encoded insn; PC_insn == q_imem
+    wire [31:0] FD_IR_in;
     wire [63:0] FD_data_in; // {seq_PC, PC_insn}
 
     /*  --- DECODE STAGE --------------------------------------------------  */
@@ -89,25 +90,39 @@ module processor(
     wire [31:0] FD_RS1VAL;
     wire [31:0] FD_RS2VAL;
 
+    // Next PC wires
+    wire ctrl_PC;
+    wire [31:0] branch_PC;
+
+    wire take_I_branch;
+    wire rd_NE_rs, rd_LT_rs;
+    wire [31:0] FD_N, I_PC;
+
+    wire [31:0] FD_T, J_PC;
+
     // DECODE STAGE output wires
-    wire [95:0] DX_data_in;
+    wire [127:0] DX_data_in;
 
     /*  --- EXECUTE STAGE -------------------------------------------------  */
     //  D/X reg read values
     //  [95:64] = D/X.RS1VAL
     //  [63:32] = D/X.RS2VAL
     //  [31:0]  = D/X.IR
-    wire [95:0] DX_data_out;
-    wire [31:0] DX_RS1VAL, DX_RS2VAL, DX_IR;
+    wire [127:0] DX_data_out;
+    wire [31:0] DX_PC, DX_RS1VAL, DX_RS2VAL, DX_IR;
 
     //  ALU Logic
-    wire [31:0] I_type_constant;
+    wire [31:0] DX_N;
+    wire [31:0] DX_T;
 
+    wire DX_ALU_overflow;
     wire [4:0] DX_ALU_opcode, DX_ALU_shamt;
     wire [31:0] DX_ALU_operandB;
-    wire [31:0] DX_ALU_output;
+    wire [31:0] DX_ALU_output; // this is raw output of ALU
 
     //  EXECUTE STAGE output wires
+    wire [31:0] XM_ALUVAL_in;     // this is final val saved to XM_reg after checking for exceptions and stuff
+    wire [31:0] XM_IR_in;
     wire [95:0] XM_data_in;
 
     /*  --- MEMORY STAGE --------------------------------------------------  */
@@ -158,14 +173,15 @@ module processor(
         // Unused Output Ports: isNotEqual, isLessThan, overflow
     );
 
-    assign PC_data_in = seq_PC; // [TODO]: change this for branch logic!!!
+    assign PC_data_in = ctrl_PC ? branch_PC : seq_PC;
     
     //  --- IMEM Logic
     assign address_imem = PC_data_out;
     assign PC_insn = q_imem;
 
     //  --- FETCH STAGE Output Logic
-    assign FD_data_in = {seq_PC, PC_insn};
+    assign FD_IR_in = ctrl_PC ? 32'd0 : PC_insn; // insert nop if take_I_branch
+    assign FD_data_in = {seq_PC, FD_IR_in};
 
     /*  --- DECODE STAGE --------------------------------------------------  */
     //  --- F/D Reg = {[63:32] F/D.PC, [31:0] F/D.IR}
@@ -182,25 +198,81 @@ module processor(
     assign FD_PC = FD_data_out[63:32];
     assign FD_IR = FD_data_out[31:0];
 
+    //  --- Branch Decoding Logic
+    // I branch logic
+    assign FD_N = {{15{FD_IR[16]}}, FD_IR[16:0]};
+
+    alu FD_ALU(
+        .data_operandA(FD_PC),
+        .data_operandB(FD_N), 
+        .ctrl_ALUopcode(5'd0),
+        .data_result(I_PC)
+        // Unused Input Ports: ctrl_shiftamt
+        // Unused Output Ports: isNotEqual, isLessThan, overflow
+    );
+
+    alu FD_COMP(
+        .data_operandA(FD_RS2VAL), // $rd, or if bex this is $r30
+        .data_operandB(FD_RS1VAL), // $rs, or if bex this is $r0
+        // Unused Input Ports: ctrl_ALUopcode, ctrl_shiftamt
+        // Unused Output Ports: data_result, overflow
+        .isNotEqual(rd_NE_rs),
+        .isLessThan(rd_LT_rs)
+    );
+
+    assign take_I_branch = (
+        (FD_IR[31:27] == 5'b00010 || FD_IR[31:27] == 5'b00110) && // is bne or blt
+        (rd_NE_rs && rd_LT_rs) // $rd < $rs
+    ) ? 1'b1 : 1'b0;
+    
+    // J branch logic
+    assign FD_T = {5'b0, FD_IR[26:0]};
+
+    assign J_PC = (
+        FD_IR[31:27] == 5'b00001 ||    // j
+        FD_IR[31:27] == 5'b00011 ||    // jal
+        (FD_IR[31:27] == 5'b10110 && rd_NE_rs) // bex and $r30 != $r0
+    ) ? FD_T : FD_RS2VAL;
+
+    // Overall branch logic
+    assign ctrl_PC = (
+        FD_IR[31:27] == 5'b00001 ||  // j
+        FD_IR[31:27] == 5'b00011 ||  // jal
+        FD_IR[31:27] == 5'b00100     // jr
+    ) ? 1'b1 : take_I_branch;
+
+    assign branch_PC = (
+        FD_IR[31:27] == 5'b00001 ||  // j
+        FD_IR[31:27] == 5'b00011 ||  // jal
+        FD_IR[31:27] == 5'b00100     // jr
+    ) ? J_PC : I_PC;
+
     //  --- Regfile Read/Write Logic
     //  output ctrl_writeEnable;
 	//  output [4:0] ctrl_writeReg, ctrl_readRegA, ctrl_readRegB;
 	//  output [31:0] data_writeReg;
 	//  input [31:0] data_readRegA, data_readRegB;
-    assign ctrl_readRegA = FD_IR[21:17];  //  THIS IS $rs addr
-    assign ctrl_readRegB = (FD_IR[31:27] == 5'd0) ? FD_IR[16:12] : FD_IR[26:22]; // $rt addr if R-type, $rd addr if NOT R-type
+    assign ctrl_readRegA = (
+        FD_IR[31:27] == 5'b10110 // if bex (10110), use $r0 instead of $rs
+    ) ? 5'd0 : FD_IR[21:17];  // THIS IS $rs addr
+    
+    assign ctrl_readRegB = (
+        FD_IR[31:27] == 5'b10110 // if bex (10110), use $r30 instead of either
+    ) ? 5'd30 : (
+        (FD_IR[31:27] == 5'd0) ? FD_IR[16:12] : FD_IR[26:22]
+    ); // $rt addr if R-type, $rd addr if NOT R-type
 	// ctrl_writeEnable, ctrl_writeReg, data_writeReg handled in writeback stage
 
 	assign FD_RS1VAL = data_readRegA;     //  THIS IS $rs val
     assign FD_RS2VAL = data_readRegB;     //  THIS IS $rt val if R-type, $rd val if not
 
     //  --- DECODE STAGE Output Logic
-    assign DX_data_in = {FD_RS1VAL, FD_RS2VAL, FD_IR};
+    assign DX_data_in = {FD_PC, FD_RS1VAL, FD_RS2VAL, FD_IR};
 
     /*  --- EXECUTE STAGE -------------------------------------------------  */
     //  --- D/X Reg = {[95:64] D/X.RS1VAL, [63:32] D/X.RS2VAL, [31:0] D/X.IR}
     pipeline_reg #(
-        .DATA_WIDTH(96)
+        .DATA_WIDTH(128)
     ) DX_reg (  
         .clk(clock),
         .en(1'b1),
@@ -209,27 +281,69 @@ module processor(
         .data_out(DX_data_out)
     );
 
+    assign DX_PC = DX_data_out[127:96];
     assign DX_RS1VAL = DX_data_out[95:64]; // always the $rs val
     assign DX_RS2VAL = DX_data_out[63:32]; // if (R) -> $rt val; if (not R) -> $rd val
     assign DX_IR = DX_data_out[31:0];
 
     //  --- ALU Logic
-    assign I_type_constant = {{15{DX_IR[16]}}, DX_IR[16:0]}; // 32-b sign-extended N
+    assign DX_N = {{15{DX_IR[16]}}, DX_IR[16:0]}; // 32-b sign-extended I-type N
+    assign DX_T = {5'b0, DX_IR[26:0]};
     assign DX_ALU_shamt = DX_IR[11:7]; // only used if the insn is a shift insn
     assign DX_ALU_opcode = (DX_IR[31:27] == 5'd0) ? DX_IR[6:2] : 5'd0; // if (R) -> DX.IR.ALUop; if (not R) -> always add
-    assign DX_ALU_operandB = (DX_IR[31:27] == 5'd0) ? DX_RS2VAL : I_type_constant; // if (R) -> use DX_RS2VAL == $rt; if (not R) -> use N
+    assign DX_ALU_operandB = (DX_IR[31:27] == 5'd0) ? DX_RS2VAL : DX_N; // if (R) -> use DX_RS2VAL == $rt; if (not R) -> use N
 
     alu DX_ALU(
         .data_operandA(DX_RS1VAL),        // always $rs val
         .data_operandB(DX_ALU_operandB),  // if (R) -> DX_RS2VAL == $rt; if (not R) -> I_type_constant = N
         .ctrl_ALUopcode(DX_ALU_opcode),   // if (R) -> ALUop; if (not R) -> add
         .ctrl_shiftamt(DX_ALU_shamt),     // only used in (R) shift insns
-        .data_result(DX_ALU_output)       // if (R) -> $rs + $rt; if (not R) -> $rs + N
+        .data_result(DX_ALU_output),       // if (R) -> $rs + $rt; if (not R) -> $rs + N
         // Unused Output Ports: isNotEqual, isLessThan, overflow
+        .overflow(DX_ALU_overflow)
     );
 
     //  --- EXECUTE STAGE Output Logic
-    assign XM_data_in = {DX_ALU_output, DX_RS2VAL, DX_IR};
+    // if it IS an overflow -> (assign to exception val) : (just use DX_ALU_output)
+    assign XM_ALUVAL_in = (DX_IR[31:27] == 5'b00011) ? ( // if jal (00011), use PC
+        DX_PC
+    ) : (
+        (DX_IR[31:27] == 5'b10101) ? ( // if setx (10101), use DX_T
+            DX_T
+        ) : ( // else, check if it is an overflow
+            (DX_ALU_overflow) ? (
+                (DX_IR[31:27] == 5'b00000 || DX_IR[31:27] == 5'b00101) ? ( // if it was an ALU type insn (r-type or addi) -> (check types) : (set to 0)
+                    (DX_IR[31:27] == 5'b00101) ? 32'd2 : (                 // if it was an addi -> (excpt == 2) : (check add, sub, mul, div)
+                        (DX_IR[6:2] == 5'b00000) ? 32'd1 : (               // if it was an add  -> (excpt == 1) : (check sub, mul, div)
+                            (DX_IR[6:2] == 5'b00001) ? 32'd3 : (           // if it was a  sub  -> (excpt == 3) : (check mul, div)
+                                (DX_IR[6:2] == 5'b00110) ? 32'd4 : 32'd5   // if it was a  mul  -> (excpt == 4) : (excpt == 5)
+                            )
+                        )
+                    )
+                ) : 32'd0       // if NOT an ALU insn, set XM_ALUVAL_in = 0 to avoid fucking with DMEM
+            ) : (
+                DX_ALU_output // keep the alu output if not a setx and not an overflow
+            )
+        )
+    );
+
+    assign XM_IR_in = (DX_IR[31:27] == 5'b00011) ? ( // if it's a jal (00011), change $rd <- $31
+        {DX_IR[31:27], 5'd31, DX_IR[21:0]}
+    ) : (
+        (DX_IR[31:27] == 5'b10101) ? ( // if it's a setx (10101), change $rd <- $30
+            {DX_IR[31:27], 5'd30, DX_IR[21:0]}
+        ) : ( // else, check if it IS an overflow -> (change $rd for the insn to be $r30) : (keep DX_IR the same)
+            (DX_ALU_overflow) ? (
+                (DX_IR[31:27] == 5'b00000 || DX_IR[31:27] == 5'b00101) ? // if it was an ALU insn, write to $r30, else nothing ("write" to $r0)
+                {DX_IR[31:27], 5'd30, DX_IR[21:0]} : // the excpt IR
+                {DX_IR[31:27], 5'd0, DX_IR[21:0]}    // else, $rd <- 0 IR
+            ) : (
+                DX_IR // keep DX_IR if not a setx and not an overflow
+            )
+        )
+    ); 
+
+    assign XM_data_in = {XM_ALUVAL_in, DX_RS2VAL, XM_IR_in};
 
     /*  --- MEMORY STAGE --------------------------------------------------  */
     //  --- X/M Reg = {[127:96] X/M.PC, [95:64] X/M.ALUVAL, [63:32] X/M.RDVAL, [31:0] X/M.IR}
@@ -278,21 +392,35 @@ module processor(
     //  --- Deciding what val to write back to regfile for {ALU insns, lw} logic
     //  if (R or addi) -> use output of ALU, XM_ALUVAL
     //  if (not, aka lw) -> use XM_lw_data
-    assign ctrl_writeEnable = ( // 1 for ALU insns and lw, 0 all else and also if $rd == $0
-        (
-            MW_IR[31:27] == 5'b00000 || // allow writes to regfile on r-type insns or
-            MW_IR[31:27] == 5'b00101 || // addi insn or
-            MW_IR[31:27] == 5'b01000    // lw insn
-        ) && // also make sure that we're not writing to register zero (not allowed)
-        (
-            MW_IR[26:22] != 5'd0
-        )
-    ) ? 1'b1 : 1'b0; 
+    assign ctrl_writeEnable = (
+        MW_IR[31:27] == 5'b10101 ||
+        MW_IR[31:27] == 5'b00011
+    ) ? ( // if setx (10101) or jal (00011), allow writes to $r30 or $r31 respectively
+        1'b1
+    ) : (
+        ( // 1 for ALU insns and lw, 0 all else and also if $rd == $0
+            (
+                MW_IR[31:27] == 5'b00000 || // allow writes to regfile on r-type insns or
+                MW_IR[31:27] == 5'b00101 || // addi insn or
+                MW_IR[31:27] == 5'b01000    // lw insn
+            ) && // also make sure that we're not writing to register zero (not allowed)
+            (
+                MW_IR[26:22] != 5'd0
+            )
+        ) ? 1'b1 : 1'b0
+    ); 
+
     assign data_writeReg = (
         MW_IR[31:27] == 5'b00000 || // use ALU output if r-type insn or
-        MW_IR[31:27] == 5'b00101    // addi insn
+        MW_IR[31:27] == 5'b00101 || // addi insn
+        MW_IR[31:27] == 5'b10101 || // setx, this will be T
+        MW_IR[31:27] == 5'b00011    // jal, this will be PC + 1
     ) ? MW_ALUVAL : MW_RDVAL;       // else, use lw data
-    assign ctrl_writeReg = MW_IR[26:22];  //  THIS IS $rd addr
+
+    // THIS IS $rd addr, 
+    // if setx it'll be $r30
+    // if jal, it'll be $r31
+    assign ctrl_writeReg = MW_IR[26:22];
 
 	/*  -------------------------------------------------------------------
      *  --- END OF DLX PROCESSOR IMPLEMENTATION                         ---
